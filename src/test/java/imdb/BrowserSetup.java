669edcf34728e543a72a7b1df22bc3d74ee889d5
A <athorvat@gmail.com>
2018-11-11 15:08:48 +0100

    optimization

--- src/test/java/imdb/BrowserSetup.java
+++ src/test/java/imdb/BrowserSetup.java
@@ -8,18 +8,16 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 public class BrowserSetup {
 
 	private static WebDriver driver;
-	private static boolean ready;
 
 	private static void setup() {
 		
 		ChromeDriverManager.getInstance().setup();
 		
 		driver = new ChromeDriver();
-		ready = true;
 	}
 	
 	static WebDriver getDriver() {
-		if (!ready)
+		if (driver == null)
 			setup();
 		return driver;
 	}
